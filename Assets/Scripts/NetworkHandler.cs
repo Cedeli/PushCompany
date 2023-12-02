17e63cef5bfd5d8cbf1acbf88ea5c761db01618e
Cedeli <96904745+Cedeli@users.noreply.github.com>
2023-12-02 23:04:11 +0800

    Refactor Networking

--- Assets/Scripts/NetworkHandler.cs
+++ Assets/Scripts/NetworkHandler.cs
@@ -8,37 +8,28 @@ public class NetworkHandler
 {
     private static GameObject pushObject;
 
-    #region Host
     [HarmonyPrefix]
-    [HarmonyPatch(typeof(MenuManager), "StartHosting")]
-    static void CacheHostNetworkHandler()
+    [HarmonyPatch(typeof(GameNetworkManager), "Start")]
+    static void Init()
     {
         NetworkManager.Singleton.AddNetworkPrefab(PushCompanyBase.pushPrefab);
     }
 
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(MenuManager), "StartHosting")]
-    static void SpawnNetworkHandler()
+    [HarmonyPatch(typeof(StartOfRound), "Awake")]
+    static void SpawnNetworkPrefab()
     {
-        pushObject = Object.Instantiate(PushCompanyBase.pushPrefab);
-
         try
         {
-            pushObject.GetComponent<NetworkObject>().Spawn();
+            if (NetworkManager.Singleton.IsServer)
+            {
+                pushObject = Object.Instantiate(PushCompanyBase.pushPrefab);
+                pushObject.GetComponent<NetworkObject>().Spawn(true);
+            }
         }
         catch
         {
             PushCompanyBase.Instance.Value.mls.LogError("Failed to instantiate network prefab!");
         }
     }
-    #endregion Host
-
-    #region Client
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(GameNetworkManager), "StartClient")]
-    static void CacheClientNetworkHandler()
-    {
-        NetworkManager.Singleton.AddNetworkPrefab(PushCompanyBase.pushPrefab);
-    }
-    #endregion Client
 }
